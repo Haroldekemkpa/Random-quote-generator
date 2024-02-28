5a112cac206491c7421c90a6b3243007d28f02fd
HaroldEkemkpa <haroldonyebuchi507@gmail.com>
2024-02-28 17:52:27 +0100

    random quote generator completed

---
+++ index.js
@@ -0,0 +1,34 @@
+let quote = ['The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela', 
+            'The way to get started is to quit talking and begin doing. -Walt Disney', 
+            "Your time is limited, so don't waste it living someone else's life.", 
+            "Don't be trapped by dogma which is living with the results of other people's thinking. -Steve Jobs",
+            'The future belongs to those who believe in the beauty of their dreams. -Eleanor Roosevelt',
+            "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. -Oprah Winfrey",
+            "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. -James Cameron",
+            "You may say I'm a dreamer, but I'm not the only one. I hope someday you'll join us. And the world will live as one. -John Lennon"
+];
+
+
+let display = document.querySelector('.quote');
+
+
+// display.innerHTML = quote[n];
+genQuote();
+
+setInterval(() => {
+    genQuote();
+}, 5000);
+
+
+
+function genQuote(){
+
+    display.innerHTML = '';
+
+    let n = Math.floor(Math.random() * quote.length);
+
+    let para = document.createElement('p');
+    para.innerHTML = quote[n];
+    display.appendChild(para);
+}
+
